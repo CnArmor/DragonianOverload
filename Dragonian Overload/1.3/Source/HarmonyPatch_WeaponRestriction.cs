5003ea7a44c171c2f9a0fa8e3ab8e7780458bf76
CnArmor <95961654+CnArmor@users.noreply.github.com>
2021-12-15 01:55:22 +0800

    Add files via upload

---
+++ Dragonian Overload/1.3/Source/HarmonyPatch_WeaponRestriction.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+using AlienRace;
+using HarmonyLib;
+
+namespace Dragonian
+{
+    [HarmonyPatch(typeof(RaceRestrictionSettings),"CanEquip")]
+    public class Patch_RaceRestrictionSettings
+    {
+        [HarmonyPostfix]
+        private static void Postfix(ref bool __result, ThingDef weapon, ThingDef race)
+        {
+            if (race == DragonianRaceDefOf.Dragonian_Female && weapon.IsMeleeWeapon)
+                __result = true;
+        }
+    }
+}
