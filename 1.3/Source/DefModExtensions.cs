cf5bcc22a9b407deccacd16f637855f7c4dcc44e
CnArmor <t3485mod44@gmail.com>
2021-12-15 03:25:14 +0800

    changed file structure
    changed structure

---
+++ 1.3/Source/DefModExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace Dragonian
+{
+    [StaticConstructorOnStartup]
+
+    public class MeleeModifier : DefModExtension
+    {
+        public float meleeDamageMultiplier = 1f;
+        public float meleeCooldownMultiplier = 1f;
+    }
+}
